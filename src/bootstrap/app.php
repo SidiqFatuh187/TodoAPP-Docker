33fe0b43c9ecdbe5b0fc533b3bbbb2c3c17a258b
SidiqFatuh187 <kmzbengbeng14@gmail.com>
2026-06-19 10:33:48 +0700

    feat: Menambah middleware untuk usertimezone dan menmabah method timzone di router dan mendaftarkan timezone di app.php

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     // Admin Middleware
     $middleware->alias([
     'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    'timezone' =>\App\Http\Middleware\SetUsertimezone::class,
     ]);
 
     })
