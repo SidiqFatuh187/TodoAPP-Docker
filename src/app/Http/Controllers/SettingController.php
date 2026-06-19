0cec7fe969d67a3eeb2c92362ddc275c178b2a63
SidiqFatuh187 <kmzbengbeng14@gmail.com>
2026-06-19 10:31:26 +0700

    feat: Menambah db timezone bila user berbeda lokasi

--- src/app/Http/Controllers/SettingController.php
+++ src/app/Http/Controllers/SettingController.php
@@ -11,4 +11,20 @@ class SettingController extends Controller
         return view('settings.index');
     }
     
+    public function update(Request $request)
+    {
+        $request->validate([
+            'timezone' => 'required|in:Asia/Jakarta,Asia/Makassar,Asia/Jayapura',
+        ]);
+
+        $user = auth()->user();
+        try {
+        $user->timezone = $request->timezone;
+        $user->save();
+
+         return redirect()->back()->with('success', 'Pengaturan berhasil disimpan.');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal menyimpan pengaturan, coba lagi.');
+        }
+    }
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'role', 'is_banned', 'avatar', 'phone', 'bio',])]
+#[Fillable(['name', 'email', 'password', 'role', 'is_banned', 'avatar', 'phone', 'bio','timezone',])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
