99a36fa853297bc53b21cb86e04fe1bc667f22e6
SidiqFatuh187 <kmzbengbeng14@gmail.com>
2026-06-22 12:30:40 +0700

    feat: menambah Schedule untuk setiap preferensi waktu

--- src/routes/console.php
+++ src/routes/console.php
@@ -9,3 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:send-task-notifications')->dailyAt('07:00');
+Schedule::command('app:send-task-notifications')->dailyAt('06:00');
+Schedule::command('app:send-task-notifications')->dailyAt('05:00');
